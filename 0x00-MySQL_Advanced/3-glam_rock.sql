b23212f1e93ef8ae152dea9fbee79cf97639682b
Akuya Ekorot <akuyaekorot@gmail.com>
2024-01-18 17:09:59 +0300

    old school band

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,9 @@
+-- lists al bands with 'Glam rock' as their main style
+-- ranked by their longevity
+
+SELECT
+  band_name,
+  (COALESCE(split, 2022) - formed) as lifespan
+FROM metal_bands
+WHERE style LIKE '%glam rock%'
+ORDER BY lifespan DESC;
