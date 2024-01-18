ffff2ad634e8e7a8130fb6bf2e3b07309e4d30d3
Akuya Ekorot <akuyaekorot@gmail.com>
2024-01-18 15:58:58 +0300

    column with enum

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,11 @@
+-- creates a table users following this requirements
+-- id: integer, primary key, autoincrement
+-- email: string(255 characters), never null and unique
+-- name: string(255 characters)
+-- if the table already exists, it should not be recreated (no error should be thrown)
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM ('US', 'CO', 'TN') DEFAULT 'US'
+);
